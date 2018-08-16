18af770b2c8608cc6dafea363c8fb777921c502e
Jose Carlos Recuero Arias <jose.recuero@gmail.com>
2018-08-16 14:19:28 -0700

    Add grafo package

---
+++ app/tnovel/grafo/leaf.go
@@ -0,0 +1,203 @@
+package grafo
+
+import (
+	"fmt"
+
+	"github.com/jrecuero/go-cli/tools"
+)
+
+// IContent represents the interface for any leaf content.
+type IContent interface {
+	GetLabel() string
+}
+
+// ClearanceCb represents ...
+type ClearanceCb func(parent *Leaf, child *Leaf, params ...interface{}) (interface{}, bool)
+
+// Ider represents ...
+type Ider uint64
+
+var _ider Ider
+
+// nextIder is ...
+func nextIder() Ider {
+	_ider++
+	return _ider
+}
+
+// Traverse represents ...
+type Traverse struct {
+	id     Ider
+	Parent *Leaf
+	Child  *Leaf
+}
+
+// NewTraverse is ...
+func NewTraverse(parent *Leaf, child *Leaf) *Traverse {
+	return &Traverse{
+		id:     nextIder(),
+		Parent: parent,
+		Child:  child,
+	}
+}
+
+// Branch is ...
+type Branch struct {
+	*Traverse
+	Clearance ClearanceCb
+}
+
+// Check is ...
+func (branch *Branch) Check(params ...interface{}) (interface{}, bool) {
+	return branch.Clearance(branch.Parent, branch.Child, params...)
+}
+
+// NewBranch is ...
+func NewBranch(parent *Leaf, child *Leaf, clearance ClearanceCb) *Branch {
+	return &Branch{
+		Traverse:  NewTraverse(parent, child),
+		Clearance: clearance,
+	}
+}
+
+// StaticBranch is ...
+func StaticBranch(parent *Leaf, child *Leaf) *Branch {
+	return NewBranch(parent, child, func(parent *Leaf, child *Leaf, params ...interface{}) (interface{}, bool) {
+		return nil, true
+	})
+}
+
+// Leaf represents ...
+type Leaf struct {
+	id        Ider
+	Label     string
+	Parents   []*Leaf
+	Branches  []*Branch
+	Traversed []*Traverse
+	Content   IContent
+	hooked    bool
+}
+
+// AddParent is ...
+func (leaf *Leaf) AddParent(parent *Leaf) error {
+	leaf.Parents = append(leaf.Parents, parent)
+	return nil
+}
+
+// AddBranch is ...
+func (leaf *Leaf) AddBranch(branch *Branch) error {
+	leaf.Branches = append(leaf.Branches, branch)
+	return nil
+}
+
+// NewLeaf is ...
+func NewLeaf(label string) *Leaf {
+	return &Leaf{
+		id:    nextIder(),
+		Label: label,
+	}
+}
+
+// Path represents ...
+type Path struct {
+	id        Ider
+	Label     string
+	Traversed []*Traverse
+}
+
+// NewPath is ...
+func NewPath(label string) *Path {
+	return &Path{
+		id:    nextIder(),
+		Label: label,
+	}
+}
+
+// Tree represents ...
+type Tree struct {
+	id     Ider
+	Label  string
+	root   *Leaf
+	anchor *Leaf
+	path   *Path
+}
+
+// GetRoot is ...
+func (tree *Tree) GetRoot() *Leaf {
+	return tree.root
+}
+
+// GetAnchor is ...
+func (tree *Tree) GetAnchor() *Leaf {
+	return tree.anchor
+}
+
+// AddBranch is ...
+func (tree *Tree) AddBranch(parent *Leaf, branch *Branch) error {
+	if parent == nil {
+		parent = tree.GetRoot()
+		branch.Parent = parent
+	}
+	if !parent.hooked {
+		return tools.ERROR(nil, false, "Parent not found in tree: %#v\n", parent)
+	}
+	if err := parent.AddBranch(branch); err != nil {
+		return err
+	}
+	child := branch.Child
+	if err := child.AddParent(parent); err != nil {
+		return err
+	}
+	child.hooked = true
+	return nil
+}
+
+// AddChild is ...
+func (tree *Tree) AddChild(parent *Leaf, child *Leaf) error {
+	if parent == nil {
+		parent = tree.GetRoot()
+	}
+	branch := StaticBranch(parent, child)
+	return tree.AddBranch(parent, branch)
+}
+
+// PathsFrom is ...
+func (tree *Tree) PathsFrom(anchor *Leaf, params ...interface{}) []*Leaf {
+	var children []*Leaf
+	for _, branch := range anchor.Branches {
+		if _, ok := branch.Check(params...); ok {
+			children = append(children, branch.Child)
+		}
+	}
+	return children
+}
+
+// SetAnchorTo is ..
+func (tree *Tree) SetAnchorTo(anchor *Leaf) *Leaf {
+	tree.anchor = anchor
+	return tree.GetAnchor()
+}
+
+// AddTraverse is ...
+func (tree *Tree) AddTraverse(branch *Branch) error {
+	if tree.GetAnchor() != branch.Parent {
+		return tools.ERROR(nil, false, "parent is not the anchor: %#v\n", branch.Parent)
+	}
+	tree.SetAnchorTo(branch.Child)
+	tree.path.Traversed = append(tree.path.Traversed, branch.Traverse)
+	return nil
+}
+
+// NewTree is ...
+func NewTree(label string) *Tree {
+	root := NewLeaf("root/0")
+	root.hooked = true
+	tree := &Tree{
+		id:    nextIder(),
+		Label: label,
+		root:  root,
+		path:  NewPath(fmt.Sprintf("%s/path", label)),
+	}
+	tree.anchor = tree.GetRoot()
+	return tree
+}
---
+++ app/tnovel/grafo/leaf_test.go
@@ -0,0 +1,115 @@
+package grafo_test
+
+import (
+	"testing"
+
+	"github.com/jrecuero/go-cli/app/tnovel/grafo"
+)
+
+// TestLeaf_NewLeaf is ...
+func TestLeaf_NewLeaf(t *testing.T) {
+	if leaf := grafo.NewLeaf("leaf/0"); leaf == nil {
+		t.Errorf("NewLeaf: leaf can not be <nil>")
+	}
+}
+
+// TestLeaf_AddBranch is ...
+func TestLeaf_AddBranch(t *testing.T) {
+	parentLeaf := grafo.NewLeaf("root/1")
+	childLeaf := grafo.NewLeaf("child/1")
+	branch := grafo.StaticBranch(parentLeaf, childLeaf)
+	if err := parentLeaf.AddBranch(branch); err != nil {
+		t.Errorf("Leaf:AddBranch: return code error: %#v\n", err)
+	}
+	if len(parentLeaf.Branches) != 1 {
+		t.Errorf("Leaf:AddBranch: branches length mismatch: exp %d got: %d\n", 1, len(parentLeaf.Branches))
+	}
+}
+
+// TestTree_NewTree is ...
+func TestTree_NewTree(t *testing.T) {
+	tree := grafo.NewTree("tree/1")
+	if tree == nil {
+		t.Errorf("NewTree: tree can not be <nil>")
+	}
+	if tree.Label != "tree/1" {
+		t.Errorf("NewTree: label mistmatch: exp: %#v got: %#v\n", "tree/1", tree.Label)
+	}
+	if tree.GetRoot() == nil {
+		t.Errorf("NewTree: root can not be <nil>")
+	}
+	if tree.GetAnchor() == nil {
+		t.Errorf("NewTree: anchor can not be <nil>")
+	}
+	if tree.GetAnchor() != tree.GetRoot() {
+		t.Errorf("NewTree: anchor mismatch: exp: %v got: %v\n", tree.GetRoot(), tree.GetAnchor())
+	}
+}
+
+// TestTree_AddBranch is ...
+func TestTree_AddBranch(t *testing.T) {
+	tree := grafo.NewTree("tree/1")
+	root := tree.GetRoot()
+	parent := grafo.NewLeaf("parent/1")
+	rootBranch := grafo.StaticBranch(nil, parent)
+	if err := tree.AddBranch(nil, rootBranch); err != nil {
+		t.Errorf("AddBranch: return code error: %#v\n", err)
+	}
+	if rootBranch.Parent == nil {
+		t.Errorf("AddBranch: branch parent mismatch: exp not <nil>")
+	}
+	if rootBranch.Parent != root {
+		t.Errorf("AddBranch: branch parent label mismatch: exp: %v got: %v\n", root, rootBranch.Parent)
+	}
+	if len(parent.Parents) != 1 {
+		t.Errorf("Tree:AddBranch: child parents length mismatch: exp %d got: %d\n", 1, len(parent.Parents))
+	}
+	if parent.Parents[0] != root {
+		t.Errorf("Tree:AddBranch: child parent mismatch: exp %v got: %v\n", root, parent.Parents[0])
+	}
+}
+
+// TestTree_AddChild is ...
+func TestTree_AddChild(t *testing.T) {
+	tree := grafo.NewTree("tree/1")
+	root := tree.GetRoot()
+	child1 := grafo.NewLeaf("child/1")
+	if err := tree.AddChild(nil, child1); err != nil {
+		t.Errorf("Tree:AddChild: return code error: %#v\n", err)
+	}
+	if len(child1.Parents) != 1 {
+		t.Errorf("Tree:AddChild: child parents length mismatch: exp %d got: %d\n", 1, len(child1.Parents))
+	}
+	if child1.Parents[0] != root {
+		t.Errorf("Tree:AddChild: child mismatch: exp %v got: %v\n", root, child1.Parents[0])
+	}
+
+	child2 := grafo.NewLeaf("child/2")
+	if err := tree.AddChild(nil, child2); err != nil {
+		t.Errorf("Tree:AddChild: return code error: %#v\n", err)
+	}
+	if len(child2.Parents) != 1 {
+		t.Errorf("Tree:AddChild: child parents length mismatch: exp %d got: %d\n", 1, len(child2.Parents))
+	}
+	if child2.Parents[0] != root {
+		t.Errorf("Tree:AddChild: child mismatch: exp %v got: %v\n", root, child2.Parents[0])
+	}
+	if len(root.Branches) != 2 {
+		t.Errorf("Tree:AdChild: root branches length mismatch: exp: %d got: %d\n", 2, len(root.Branches))
+	}
+}
+
+// TestTree_PathsFrom is ...
+func TestTree_PathsFrom(t *testing.T) {
+	tree := grafo.NewTree("tree/1")
+	parent := grafo.NewLeaf("parent/1")
+	child1 := grafo.NewLeaf("child/1")
+	child2 := grafo.NewLeaf("child/2")
+	tree.AddChild(nil, parent)
+	tree.AddChild(parent, child1)
+	tree.AddChild(parent, child2)
+	paths := tree.PathsFrom(parent)
+	if len(paths) != 2 {
+		t.Errorf("Tree:PathsFrom: pathd length mismatch: exp: %d got: %d\n", 2, len(paths))
+	}
+}
